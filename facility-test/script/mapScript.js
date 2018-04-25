a0f736f7574f17a87739764fb39ef807d6aa7e94
makivaras <domovoy415@gmail.com>
2018-04-25 12:16:16 +0300

    revert

--- facility-test/script/mapScript.js
+++ facility-test/script/mapScript.js
@@ -1,11 +1,22 @@
+<<<<<<< HEAD
 function initMap() {
     var uluru = {lat: 50.4506819, lng: 30.5230936};
     var map = new google.maps.Map(document.getElementById('map'), {
+=======
+navigator.geolocation.getCurrentPosition(updatePosition);
+
+function updatePosition(pos) {
+    // задание координат.
+    var position = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+
+    // параметры для карты.
+    var options = {
+>>>>>>> parent of e02e1a9... some fix
         zoom: 10,
-        center: uluru
-    });
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-    });
+        center: position, // позиция на карте
+        mapTypeId: google.maps.MapTypeId.ROADMAP // тип карты - ROADMAP, SATELLITE, HYBRID and TERRAIN
+    };
+
+    // объект карты.
+    var map = new google.maps.Map(document.getElementById("map"), options);
 }
