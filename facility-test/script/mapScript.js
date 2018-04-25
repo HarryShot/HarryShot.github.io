f1bbd190fcaee763939c01f5bbf93d738062156a
makivaras <domovoy415@gmail.com>
2018-04-25 12:22:19 +0300

    add

--- facility-test/script/mapScript.js
+++ facility-test/script/mapScript.js
@@ -1,3 +1,12 @@
+var posLat = 50.4506819;
+var posLng = 30.5230936;
+
+navigator.geolocation.getCurrentPosition(function(position) {
+    // Текущие координаты.
+    posLat = position.coords.latitude;
+    posLng = position.coords.longitude;
+    initMap();
+});
 
 function initMap() {
     var uluru = {lat: 50.4506819, lng: 30.5230936};
